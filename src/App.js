5a91f7c7340d088b741f7a17408a42ad499f19cc
Harsh4221 <harshpatel422118@gmail.com>
2022-08-17 18:10:00 +0530

    edit react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 /* eslint-disable react/react-in-jsx-scope */
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 function App() {
-  return <div className="App">Hi</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        This is Home.
+      </Route>
+      <Route exact path="/starred">
+        This is starred.
+      </Route>
+      <Route>This is 404 Page.</Route>
+    </Switch>
+  );
 }
 
 export default App;
