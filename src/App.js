310dffeea6ca0f29f4c5c1e827b6999e76c0eb67
Harsh4221 <harshpatel422118@gmail.com>
2022-08-19 14:51:35 +0530

    edit show page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/react-in-jsx-scope */
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-
+import Show from './pages/Show';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 
@@ -13,6 +13,9 @@ function App() {
       </Route>
       <Route exact path="/starred">
         <Starred />
+      </Route>
+      <Route exact path="/show/:id">
+        <Show />
       </Route>
       <Route>
         <div>Not Found</div>
---
+++ src/pages/Show.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/function-component-definition */
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { apiGet } from '../misc/config';
+
+const Show = () => {
+  const { id } = useParams();
+  const [show, setShow] = useState(null);
+
+  useEffect(() => {
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
+      setShow(results);
+    });
+  }, [id]);
+  console.log(show);
+  return <div>show</div>;
+};
+
+export default Show;
