bc1a2a1987add587fac1d17705be230539407ebc
Harsh4221 <harshpatel422118@gmail.com>
2022-08-22 12:51:47 +0530

    Final commit

--- src/components/show/ShowGrid.jsx
+++ src/components/show/ShowGrid.jsx
@@ -8,21 +8,21 @@ import { FlexGrid } from '../styled';
 import { useShows } from '../../misc/custom-hooks';
 
 const ShowGrid = ({ data }) => {
-  const [starredShows, dispatcheStarred] = useShows();
+  const [starredShows, dispatchStarred] = useShows();
+  const onStarClick = useCallback(
+    (showId, isStarred) => {
+      if (isStarred) {
+        dispatchStarred({ type: 'REMOVE', showId });
+      } else {
+        dispatchStarred({ type: 'ADD', showId });
+      }
+    },
+    [dispatchStarred]
+  );
 
   return (
     <FlexGrid>
       {data.map(({ show }) => {
-        const isStarred = starredShows.includes(show.id);
-
-        const onStarClick = useCallback(() => {
-          if (isStarred) {
-            dispatcheStarred({ type: 'REMOVE', showId: show.id });
-          } else {
-            dispatcheStarred({ type: 'ADD', showId: show.id });
-          }
-        }, [isStarred, show.id]);
-
         return (
           <ShowCard
             key={show.id}
@@ -31,7 +31,7 @@ const ShowGrid = ({ data }) => {
             image={show.image ? show.image.medium : IMAGE_NOT_FOUND}
             summary={show.summary}
             onStarClick={onStarClick}
-            isStarred={isStarred}
+            isStarred={starredShows.includes(show.id)}
           />
         );
       })}
